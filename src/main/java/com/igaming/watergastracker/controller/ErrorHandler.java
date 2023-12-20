e6bf0885f3702788c7ebda31f648078b88552ce4
aleksandr-dudko <aleksandr.dudko@akvelon.com>
2023-12-20 17:06:54 +0400

    Update custom exceptions handling.

--- src/main/java/com/igaming/watergastracker/controller/ErrorHandler.java
+++ src/main/java/com/igaming/watergastracker/controller/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -31,14 +33,23 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
      * @return A ResponseEntity with a Bad Request status and error message.
      */
     @ExceptionHandler({RuntimeException.class})
-    public final ResponseEntity<String> handleRuntimeAndMethodArgumentNotValidException(final Exception ex,
-                                                                                        final WebRequest request) {
+    public final ResponseEntity<String> handleRuntimeException(final Exception ex,
+                                                               final WebRequest request) {
         LOGGER.log(Level.ERROR, ex);
         String errorMessage = "Error: " + ex.getMessage();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 
+    /**
+     * Custom exception handler for handling MethodArgumentNotValidException.
+     *
+     * @param ex      The MethodArgumentNotValidException instance.
+     * @param headers The HTTP headers for the response.
+     * @param status  The HTTP status for the response.
+     * @param request The web request.
+     * @return A ResponseEntity containing the error response.
+     */
     public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                HttpHeaders headers, HttpStatus status, WebRequest request) {
         List<String> errors = new ArrayList<>();
@@ -46,6 +57,21 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
                 errors.add(error.getField() + ": " + error.getDefaultMessage()));
 
         ErrorResponse errorResponse = new ErrorResponse("Validation failed", errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Custom exception handler for handling HttpMessageNotReadableException.
+     *
+     * @param ex      The HttpMessageNotReadableException instance.
+     * @param headers The HTTP headers for the response.
+     * @param status  The HTTP status for the response.
+     * @param request The web request.
+     * @return A ResponseEntity containing the error response.
+     */
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                               HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse("Malformed JSON request", Collections.singletonList(ex.getMessage()));
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
