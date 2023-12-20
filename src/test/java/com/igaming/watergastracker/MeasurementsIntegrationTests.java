8057a776f03be8d47ddc72344ac0a319ceb3d1a3
aleksandr-dudko <aleksandr.dudko@akvelon.com>
2023-12-20 17:06:23 +0400

    Add more tests. Coverage 80%.

--- src/test/java/com/igaming/watergastracker/MeasurementsIntegrationTests.java
+++ src/test/java/com/igaming/watergastracker/MeasurementsIntegrationTests.java
@@ -11,6 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MeasurementsIntegrationTests {
@@ -26,6 +29,53 @@ public class MeasurementsIntegrationTests {
                 .hotWaterUsage(15.0)
                 .build();
 
+        submitMeasurement(measurement);
+    }
+
+    @Test
+    public void submitMeasurementWithEmptyUserId() throws Exception {
+        Measurement measurement = Measurement.builder()
+                .userId("")
+                .gasUsage(25.5)
+                .coldWaterUsage(10.0)
+                .hotWaterUsage(15.0)
+                .build();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/measurements/submit")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(measurement)))
+                .andExpect(jsonPath("$.message").value("Validation failed"))
+                .andExpect(jsonPath("$.errors[0]").value("userId: User ID cannot be blank"));
+    }
+
+    @Test
+    public void getMeasurementsHistory() throws Exception {
+        int userIdRecordsNum = 100;
+
+        for (int i = 0; i < userIdRecordsNum; i++) {
+            submitMeasurement(Measurement.builder()
+                    .userId("testUser")
+                    .gasUsage(i + 1)
+                    .coldWaterUsage(i + 100)
+                    .hotWaterUsage(i + 10)
+                    .build());
+
+            submitMeasurement(Measurement.builder()
+                    .userId("testUser101")
+                    .gasUsage(25.5)
+                    .coldWaterUsage(10.0)
+                    .hotWaterUsage(15.0)
+                    .build());
+        }
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/measurements/history/testUser")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$", hasSize(userIdRecordsNum)));
+
+    }
+
+    private void submitMeasurement(Measurement measurement) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/measurements/submit")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(measurement)))
